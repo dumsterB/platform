e4eb8510a4e8be192d6423d06963c41d9c20ae05
Tair <abduraimovtair@gmail.com>
2022-05-04 17:09:52 +0300

    add nots history

--- components/layout/NotHistory.vue
+++ components/layout/NotHistory.vue
@@ -6,34 +6,32 @@
       :key="i"
       class="not-block mb-4 px-5 py-3"
     >
-      <span>
-        <v-badge class="ml-1 mr-2" color="#E20000"></v-badge
-        ><span class="not-title ml-4">{{ not.title }}</span>
-      </span>
+      <v-row>
+        <v-col :cols="9">
+          <v-badge class="ml-1 mr-2" color="#E20000"></v-badge
+          ><span class="not-title ml-4">{{ not.title }}</span>
+        </v-col>
+        <v-col class="text-right" :cols="3">
+          <span class="note-date">{{ not.ago }}</span>
+        </v-col>
+      </v-row>
       <p class="not-text mt-2">{{ not.text }}</p>
     </div>
   </v-card>
 </template>
 <script>
 import { mapGetters, mapActions, mapMutations } from "vuex";
+import moment from "moment";
 
 export default {
   data() {
     return {
-      notifications: [
-        {
-          title: "Welcome to Platform",
-          text: "Welcome to Platform. Trade cryptocurrencies here.",
-        },
-        {
-          title: "Welcome to Platform",
-          text: "Welcome to Platform. Trade cryptocurrencies here.",
-        },
-        {
-          title: "Welcome to Platform",
-          text: "Welcome to Platform. Trade cryptocurrencies here.",
-        },
-      ],
+      //   notifications: [
+      //     {
+      //       title: "Welcome to Platform",
+      //       text: "Welcome to Platform. Trade cryptocurrencies here.",
+      //     },
+      //   ],
     };
   },
   methods: {
@@ -50,11 +48,16 @@ export default {
     ...mapGetters("data/notifications_history", {
       not_list: "list",
     }),
+    notifications() {
+      return this.not_list.map((el) => {
+        el.ago = moment(el.created_at).fromNow(true);
+        return el;
+      });
+    },
   },
   mounted() {},
   async created() {
     await this.fetchNots();
-    console.log('this.not_list', this.not_list);
   },
 };
 </script>
@@ -71,5 +74,9 @@ export default {
 .not-text {
   font-weight: 400;
   font-size: 12px;
+}
+.note-date {
+  font-weight: 300;
+  font-size: 10px;
 }
 </style>
